d160393c8e7fffb2f98f121cf137d2a10499e56a
Alex Li <lialex0218@gmail.com>
2021-06-02 01:18:23 +0000

    print each char each line 50ms

---
+++ typewriter.js
@@ -0,0 +1,21 @@
+const sentence = "hello there from lighthouse labs";
+
+for (const char of sentence) {
+  setTimeout(() => {
+  process.stdout.write(char);
+  process.stdout.write('\n');
+}, 50 * char)
+}
+
+
+
+// const sentence = "hello there from lighthouse labs";
+//   for (let i = 0; i < sentence.length; i++) {
+//       setTimeout(function () {
+//           process.stdout.write(sentence[i]);
+//           process.stdout.write('\n');
+//       }, i * 50);
+//   }
+
+
+
